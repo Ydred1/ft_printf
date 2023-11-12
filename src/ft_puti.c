f0100027c7d83a08d1aae7aaf199b6cc2e58aedb
nmillier <nmillier@student.42mulhouse.fr>
2023-11-12 18:37:00 +0100

    added negative integers support

--- src/ft_puti.c
+++ src/ft_puti.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_puti.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydred <ydred@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nmillier <nmillier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:41:56 by ydred             #+#    #+#             */
-/*   Updated: 2023/11/12 03:17:07 by ydred            ###   ########.fr       */
+/*   Updated: 2023/11/12 13:38:33 by nmillier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,27 @@
 
 char	*ft_puti(va_list args)
 {
-	char	c[11];
+	char	c[12];
 	int		n;
-	int 	i;
+	int		i;
+	int		nbr;
 
 	n = va_arg(args, int);
-	i = 9;
-	ft_memset(c, ' ', 11);
+	nbr = n;
+	i = 10;
+	ft_memset(c, ' ', 12);
+	c[11] = '\0';
 	c[i] = '0';
-	while (n >= 1)
+	while (n >= 1 || n <= -1)
 	{
-		c[i] = n % 10 + 48;
+		if (nbr < 0)
+			c[i] = (n % 10) * -1 + 48;
+		else
+			c[i] = n % 10 + 48;
 		n /= 10;
 		i--;
 	}
+	if (nbr < 0)
+		c[i] = '-';
 	return (ft_strtrim(c, " "));
 }
