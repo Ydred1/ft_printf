19a48d3f3dde04651ff8319c40eb8247f1f26818
nmillier <nmillier@student.42mulhouse.fr>
2023-11-15 12:15:24 +0100

    changed name to fix compiling error on mac

---
+++ src/ft_putxlower.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putx.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmillier <nmillier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 19:37:45 by ydred             #+#    #+#             */
+/*   Updated: 2023/11/12 18:31:58 by nmillier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+char	*ft_putbase16(unsigned int n, char *base)
+{
+	char			c[9];
+	int				i;
+	unsigned int	nbr;
+
+	i = 8;
+	ft_memset(c, ' ', 8);
+	c[i--] = '\0';
+	c[i] = '0';
+	nbr = n;
+	while (nbr > 1)
+	{
+		c[i] = base[nbr % 16];
+		nbr /= 16;
+		i--;
+	}
+	return (ft_strtrim(c, " "));
+}
+
+char	*ft_putxlower(va_list args)
+{
+	return (ft_putbase16(va_arg(args, unsigned int), "0123456789abcdef"));
+}
